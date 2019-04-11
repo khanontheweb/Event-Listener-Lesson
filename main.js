76ea790f3198c4db68a34c55dd1884b624ac4eac
Momo Khan <snillmomo@gmail.com>
2019-04-11 05:42:46 -0400

    Added resulting button

---
+++ main.js
@@ -0,0 +1,7 @@
+// Select the HTML element
+const buttonExample = document.getElementById("btn");
+
+//Add a handler, specifying first the event, and then the function (behavior) you want
+buttonExample.addEventListener('click', function(event) {
+    alert("Hello from button!");
+});
