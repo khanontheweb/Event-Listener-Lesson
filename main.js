13157e68e6bcf384cd6413aa17fb78986af7cb3b
Momo Khan <snillmomo@gmail.com>
2019-04-11 05:53:01 -0400

    Added bootstrap and styling to code blocks.

--- main.js
+++ main.js
@@ -4,4 +4,5 @@ const buttonExample = document.getElementById("btn");
 //Add a handler, specifying first the event, and then the function (behavior) you want
 buttonExample.addEventListener('click', function(event) {
     alert("Hello from button!");
+    console.log(event);
 });
