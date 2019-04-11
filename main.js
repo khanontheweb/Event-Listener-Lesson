77e2e3f0f130fd609102fe6a616580ee13c1b584
Momo Khan <snillmomo@gmail.com>
2019-04-11 06:08:50 -0400

    Add a little more complicated example

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
 // Select the HTML element
 const buttonExample = document.getElementById("btn");
 
-//Add a handler, specifying first the event, and then the function (behavior) you want
+// Add a handler, specifying first the event, and then the function (behavior) you want
 buttonExample.addEventListener('click', function(event) {
     alert("Hello from button!");
     console.log(event);
+});
+
+// Select the button
+const spoiler = document.getElementById("spoiler");
+
+// Handler that toggles the hidden class
+spoiler.addEventListener('click', function(event) {
+    const spoilerText = document.getElementById("spoiler-text");
+    spoilerText.classList.toggle("hidden");
 });
